528b07d1277d89fed284606dd2cd7b6c2c72a466
DoriDoro <dorothea.hassan@gmail.com>
2022-08-23 23:18:39 +0200

    initial - start dj project, scraping initialisation and saveing

---
+++ webscrap_dw_project/webscrap_dw_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+'''
+title 
+link 
+description 
+published_date 
+
+
+class Headline(models.Model):
+  title = models.CharField(max_length=200)
+  image = models.URLField(null=True, blank=True)
+  url = models.TextField()
+  def __str__(self):
+    return self.title'''
+
+
+
